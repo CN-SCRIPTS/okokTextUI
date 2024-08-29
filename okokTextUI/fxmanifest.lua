6d0d1bd0ef150cca368e7c6ba97f373a97bc8ff2
CN SCRIPTS <rachad9001@gmail.com>
2024-08-29 13:27:22 +0200

    Add files via upload

---
+++ okokTextUI/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'okok'
+description 'okokTextUI'
+version '1.0'
+
+ui_page 'web/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'web/*.*'
+}
+
+export 'Open'
+export 'Close'
